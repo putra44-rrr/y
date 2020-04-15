76c33231e0491d146838541c90f5c1b7f55bb934
putra44-rrr <62072422+putra44-rrr@users.noreply.github.com>
2020-04-15 10:47:34 +0700

    yy

--- newjek.php
+++ newjek.php
@@ -28,10 +28,10 @@ function change(){
         $uuid = getStr('"resource_owner_id":',',',$verif);
         echo "\n".color("yellow","+] Your access token : ".$token."\n\n");
         save("token.txt",$token);
-        echo "\n".color("nevy","?] Mau Redeem Voucher?: y/n ");
+        echo "\n".color("nevy","?] KAMU KANG OPIK YA?: y/n ");
         $pilihan = trim(fgets(STDIN));
         if($pilihan == "y" || $pilihan == "Y"){
-        echo color("red","===========(REDEEM VOUCHER)===========");
+        echo color("red","===========(PERSIAPAN NGOPIK)===========");
         echo "\n".color("yellow","!] Claim voc GORIDE 8K");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
@@ -52,7 +52,7 @@ function change(){
         echo color("yellow",".");
         sleep(20);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
@@ -71,7 +71,7 @@ function change(){
         }
         gofood:
         echo "\n".color("yellow","!] Claim voc GOCAR pot 14K");
-        echo "\n".color("yellow","!] Please wait");
+        echo "\n".color("yellow","!] UDAH BERES NIH , YUK LOGIN LALU KITA NGOPIK");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
         sleep(3);
