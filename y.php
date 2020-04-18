732114926d59697812608dc6d37ed5f568398e55
putra44-rrr <62072422+putra44-rrr@users.noreply.github.com>
2020-04-19 04:28:59 +0700

    Update y.php

--- y.php
+++ y.php
@@ -52,7 +52,7 @@ function change(){
         echo color("yellow",".");
         sleep(20);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"KIRIMGOSEND10"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD160420A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
